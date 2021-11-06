aaeb2784cdc92375a3801ca5d28cd9f5daf0103f
cahuc <cahuc@DESKTOP-HCUCG4H>
2021-11-05 22:00:28 -0500

    Product example collection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     StudentModule,
     MongooseModule.forRoot('mongodb+srv://root:root@cluster0.frpti.mongodb.net/db_gr_31', {
       useNewUrlParser: true
-    })
+    }),
+    ProductModule
   ],
   controllers: [AppController],
   providers: [AppService],
